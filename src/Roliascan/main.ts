fdcfe2aa782576ed2223b92e8b620a8c184400ce
Saw_6 <57862464+Thinkaz@users.noreply.github.com>
2025-12-14 22:21:39 +0100

    feat: Add Roliascan (#5)

---
+++ src/Roliascan/main.ts
@@ -0,0 +1,25 @@
+import { AnimaceGeneric } from "../generic/main";
+import pbconfig from "./pbconfig";
+
+const DOMAIN = "https://roliascan.com";
+
+class RoliascanExtension extends AnimaceGeneric {
+    constructor() {
+        super({
+            domain: DOMAIN,
+            name: pbconfig.name,
+            contentRating: pbconfig.contentRating,
+            language: pbconfig.language,
+            excludeImagePatterns: [
+                "roliascan.com/wp-content/uploads/2024/07/warning-1.png",
+                "roliascan.com/wp-content/uploads/2025/09/end-chapter.jpg",
+            ],
+            basicRateLimiter: {
+                numberOfRequests: 4,
+                bufferInterval: 1,
+            },
+        });
+    }
+}
+
+export const Roliascan = new RoliascanExtension();
